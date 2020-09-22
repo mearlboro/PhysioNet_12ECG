664e6e97c0cc2c01ee408b58b59457a295b67720
M <mearlboro@protonmail.com>
2020-09-22 23:00:54 +0100

    Add init function to turn ecg features dict into dataframe

--- get_12ECG_features.py
+++ get_12ECG_features.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import numpy as np
+from pandas import DataFrame
 from typing import Any, Dict, List
 
 from info_tools import get_info_measures
+
 
 def get_12ECG_metadata(
         header: List[str]
@@ -33,14 +35,17 @@ def get_12ECG_metadata(
         A0003.mat 16+24 1000/mV 16 0 -14 24 0 V6
         #Age: 81
         #Sex: Female
-        #Dx: 164889003
+        #Dx: 164889003,59118001
         #Rx: Unknown
         #Hx: Unknown
         #Sx: Unknown
 
     Returns
     ------
-    the relevant metadata in the form of a dictionary
+    the relevant metadata in the form of a dictionary, for example
+
+        {'subject': 'A0957', 'sample_rate': 500, 'age': 64, 'female': 0,
+         'male': 1, 'conds': '164889003,59118001'}
     """
     meta_dict = dict()
 
@@ -61,7 +66,7 @@ def get_12ECG_metadata(
             meta_dict['female'] = 1 if value == 'Female' else 0
             meta_dict['male']   = 1 if value == 'Male'   else 0
         elif line.startswith('#Dx'):
-            meta_dict['label'] = value.split(',')[0]
+            meta_dict['conds'] = value
 
     return meta_dict
 
@@ -89,9 +94,10 @@ def get_12ECG_features(
     which will be properly typed for xgboost in the preprocessing phase
     """
     meta_dict = get_12ECG_metadata(header)
+    fs = meta_dict['sample_rate']
 
     # for very long recordings, only extract features from the first 60 sec
-    T = int(60 * meta_dict['sample_rate'])
+    T = int(60 * fs)
     data = data.T
     if (len(data) > T):
         data = data[:T]
@@ -99,9 +105,38 @@ def get_12ECG_features(
     # construct feature dictionary
     feats_dict = dict()
 
-    feats_dict.update({ k: meta_dict[k] for k in ['age', 'male', 'female' ] })
-
-    info_dict = get_info_measures(data, meta_dict['sample_rate'])
+    info_dict = get_info_measures(data, fs)
     feats_dict.update(info_dict)
 
+    # include patient metadata as well as subject ID (same as file name)
+    feats_dict.update({ k: meta_dict[k]
+                        for k in ['age', 'male', 'female', 'subject'] })
+
     return feats_dict
+
+
+def init_12ECG_features(feats_dict: Dict[str, np.float64]) -> DataFrame:
+    """
+    Given a dictionary with feature names as keys and features from a
+    single recording as values, initialises a pandas dataframe in the
+    format needed to be used as input by the XGBoost model
+
+    Params
+    ------
+    feats_dict
+        feature dictionary
+
+    Returns
+    ------
+    DataFrame with features
+    """
+    # subject will be the index of dataframe, remove from dict
+    subject = feats_dict['subject']
+    del feats_dict['subject']
+
+    df = DataFrame(
+        np.asarray(list(feats_dict.values()), dtype = np.float64).reshape(1, -1),
+        columns = list(feats_dict.keys()), index = [subject])
+
+    return df
+
